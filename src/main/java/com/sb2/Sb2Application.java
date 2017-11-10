d32f6d2effadd88636db35f18995bef8adb51efb
chechelvitalii <chechelvitaliy@gmail.com>
2017-11-10 23:34:43 +0200

    hystrix stream + dashboard

--- src/main/java/com/sb2/Sb2Application.java
+++ src/main/java/com/sb2/Sb2Application.java
@@ -2,13 +2,15 @@ package com.sb2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
-@EnableCircuitBreaker
+@EnableHystrixDashboard
+@EnableHystrix
 @SpringBootApplication
 public class Sb2Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Sb2Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Sb2Application.class, args);
+    }
 }
